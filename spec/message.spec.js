3946b2b9e1fb33f6bc1c1144c65fc958cfd694f5
Ieva Swanson <ievaswanson@u.boisestate.edu>
2024-02-21 17:26:44 -0600

    Adjusted indentation in message.spec and put in test structure and describe messages for tests 7-13 in rover.spec

--- spec/message.spec.js
+++ spec/message.spec.js
@@ -12,20 +12,20 @@ describe("Message class", function() {
   
   });
 
-  describe("Message class", function() {
+describe("Message class", function() {
 
-    it("constructor sets name", function() {
-      let testMessageName = new Message("Hi there!");
-      expect(testMessageName.name).toBe("Hi there!");
-    });
-  
+  it("constructor sets name", function() {
+    let testMessageName = new Message("Hi there!");
+    expect(testMessageName.name).toBe("Hi there!");
   });
+  
+});
 
-  describe("Message class", function() {
+describe("Message class", function() {
 
-    it("contains a commands array passed into the constructor as the 2nd argument", function() {
+  it("contains a commands array passed into the constructor as the 2nd argument", function() {
       let testMessageCommands = new Message("Hi there!", ["Take a coffee break!", "Take a lunch break!", "Knock off for the day, it's late!"]);
       expect(testMessageCommands.commands[2]).toBe("Knock off for the day, it's late!");
-    });
+  });
   
-  }); 
+}); 
--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -2,12 +2,57 @@ const Rover = require('../rover.js');
 const Message = require('../message.js');
 const Command = require('../command.js');
 
-// NOTE: If at any time, you want to focus on the output from a single test, feel free to comment out all the others.
-//       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
+// NOTE: If at any time, you want to focus on the output from a single test, 
+// feel free to comment out all the others.
+// However, do NOT edit the grading tests for any reason and make sure 
+// to un-comment out your code to get the autograder to pass.
 
 
 describe("Rover class", function() {
+  it("constructor sets position and default values for mode and generatorWatts", function() {
+  // test 7
+  });
 
-  // 7 tests here!
+});
+
+describe("Rover class", function() {
+  it("response returned by receiveMessage contains the name of the message", function() {
+  // test 8
+  });
+
+});
+
+describe("Rover class", function() {
+  it("response returned by receiveMessage includes two results if two commands are sent in the message", function() {
+  // test 9
+  });
+
+});
+
+describe("Rover class", function() {
+  it("responds correctly to the status check command", function() {
+  // test 10
+  });
+
+});
+
+describe("Rover class", function() {
+  it("responds correctly to the mode change command", function() {
+  // test 11
+  });
+
+});
+
+describe("Rover class", function() {
+it("responds with a false completed value when attempting to move in LOW_POWER mode", function() {
+  // test 12
+  });
+
+});
+
+describe("Rover class", function() {
+  it("responds with the position for the move command", function() {
+  // test 13
+  });
 
 });
